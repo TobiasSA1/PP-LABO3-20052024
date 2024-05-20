65960c7627d965f0d22239535eebd73c32075ac5
TobiasSA1 <tobiassuarez43@gmail.com>
2024-05-20 20:25:31 +0200

    Segundo Push

--- Parcial/js/index.js
+++ Parcial/js/index.js
@@ -246,6 +246,10 @@ function handlerEditar() {
   const btn = document.getElementById("btnEditar");
 
   btn.addEventListener("click", async (e) => {
+    
+    if (!validarFormulario(formulario)) {
+      return;
+    }
 
     e.preventDefault();
     
